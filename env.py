7d26b519156cec00d8263d132cda3f9fe69b5697
andershup <anders242@gmail.com>
2020-09-24 07:30:49 +0000

    testcommit

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+os.environ["SECRET_KEY"] = "gis(yx3w0aav9*z11l23_sb53x-#znmcqv+%h+grz6vwua5w#p"
+os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51HTj2cCi7YLtVGm0pWL4D5Q5sk08Gh2knTN9EGqcxOhDfs9TAqwWi3Cle4HHsDyQoOg843xbuar1vub2G810qMCQ00s6p7kIym"
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51HTj2cCi7YLtVGm0piSf88cPmmynBOHjGhMI4iE3sidNLaX7fVOFWMF2JcvIcH2SmYjR4ucqtklw72IEI0O1NtB0005wbwsqbF"
+os.environ["STRIPE_WH_SECRET"] = "pk_test_51HTj2cCi7YLtVGm0pWL4D5Q5sk08Gh2knTN9EGqcxOhDfs9TAqwWi3Cle4HHsDyQoOg843xbuar1vub2G810qMCQ00s6p7kIym"
+os.environ["DEVELOPMENT"] = "True"
